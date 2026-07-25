4f6fc27aabc1d64cd943bb6fbfc692cc2ff314ab
jyh928578@gmail.com <jyh928578@gmail.com>
2026-07-25 18:12:54 +0900

    개인 개발환경 application.properties

---
+++ src/main/java/kr/co/ictedu/projectBack/vo/StockVO.java
@@ -0,0 +1,26 @@
+package kr.co.ictedu.projectBack.vo;
+
+import java.util.List;
+
+import org.apache.ibatis.type.Alias;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Alias("svo")
+@Getter
+@Setter
+public class StockVO {
+	private Integer snum;
+	private String sisnb;
+	private String sname;
+	private String spublisher;
+	private String sauthor;
+	private int samount;
+	private int sprice;
+	private String scategory;
+	private String sdate;
+	private int membernum;
+	// 1:N 즉 collection관계
+	private List<StockImagesVO> getimglist;
+}
